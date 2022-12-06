377f0cb6d1eeda8fd391bd2cab8dc190d5470f6a
solenerichard69 <solene.richard1@gmail.com>
2022-12-06 16:45:44 +0100

    loggin test

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ gtag('config', 'G-BKV9CF4L6E');
 </script>
 """
  return prefix_google + "Hey World"
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = get_user(request.form['username'])
+
+    if user.check_password(request.form['password']):
+        login_user(user)
+        app.logger.info('%s logged in successfully', user.username)
+        return redirect(url_for('index'))
+    else:
+        app.logger.info('%s failed to log in', user.username)
+        abort(401)
