9465063c9386a19d3263dbea4c8f79a87a160a1a
solenerichard69 <solene.richard1@gmail.com>
2022-12-06 16:53:42 +0100

    changement method get

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-
 def hello_world():
  prefix_google = """
 <!-- Google tag (gtag.js) -->
@@ -17,6 +17,21 @@ gtag('config', 'G-BKV9CF4L6E');
 </script>
 """
  return prefix_google + "Hey World"
+
+@app.route('/print', methods=["GET"])
+def printMsg():
+    app.logger.warning('testing warning log')
+    app.logger.error('testing error log')
+    app.logger.info('testing info log')
+    return "Check your console"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#@app.route('/logger', methods=['GET'])
+#def login():
+
 
 @app.route('/login', methods=['POST'])
 def login():
