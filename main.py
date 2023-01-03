e2fde4f1bb159e5022159e1bb9e096480bc4fbc4
solenerichard69 <60784928+solenerichard69@users.noreply.github.com>
2023-01-03 18:46:56 +0100

    Update main.py
    add logger

--- main.py
+++ main.py
@@ -18,29 +18,23 @@ gtag('config', 'G-BKV9CF4L6E');
 """
  return prefix_google + "Hey World"
 
-@app.route('/print', methods=["GET"])
+@app.route('/logger', methods=['GET','POST'])
+
 def printMsg():
     app.logger.warning('testing warning log')
     app.logger.error('testing error log')
     app.logger.info('testing info log')
     return "Check your console"
 
+@app.route('/cookie', methods=['GET', 'POST'])
+def cookie():
+    req=requests.get('https://www.google.com/')
+    return req.cookies.get_dict()
+
+@app.route('/cookie/analytics', methods=['GET', 'POST'])
+def cookie_analytics():
+    req=requests.get('https://analytics.google.com/analytics/web/?hl=fr#/p345017433/reports/reportinghub')
+    return req.text
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-#@app.route('/logger', methods=['GET'])
-#def login():
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    user = get_user(request.form['username'])
-
-    if user.check_password(request.form['password']):
-        login_user(user)
-        app.logger.info('%s logged in successfully', user.username)
-        return redirect(url_for('index'))
-    else:
-        app.logger.info('%s failed to log in', user.username)
-        abort(401)
