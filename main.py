7b94afc4a971874946f8f60c0788b247687ba34d
solenerichard69 <solene.richard1@gmail.com>
2023-01-04 12:28:00 +0100

    cxw

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import logging
-from flask import Flask
+from flask import Flask, render_template,redirect, url_for
+import requests
+from pytrends.request import TrendReq
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
+
+pytrends = TrendReq(hl='en-US', tz=360)
+kw_list = ["Maria Carey", "michael bublé", "SIA"]
+pytrends.build_payload(kw_list, timeframe='now 90-d', geo='', gprop='')
+df = pytrends.interest_over_time()
+print(df)
 
 @app.route('/', methods=["GET"])
 def hello_world():
@@ -16,25 +24,33 @@ gtag('js', new Date());
 gtag('config', 'G-BKV9CF4L6E');
 </script>
 """
- return prefix_google + "Hey World"
+ return prefix_google + "Hey You :) "
 
-@app.route('/logger', methods=['GET','POST'])
-
+@app.route('/logger', methods=['GET'])
 def printMsg():
-    app.logger.warning('testing warning log')
-    app.logger.error('testing error log')
-    app.logger.info('testing info log')
-    return "Check your console"
+    loginfo = "Here is my log!"
+    app.logger.warning(loginfo)
+    app.logger.error(loginfo)
+    app.logger.info(loginfo)
+
+    return render_template('log.html', loginfo = "Here is my log")
+
+@app.route("/chart")
+def info():
+    app.logger.info(f"Here is your chart: {df}")
+    return f"Here is your chart: \n {df}"
 
 @app.route('/cookie', methods=['GET', 'POST'])
 def cookie():
     req=requests.get('https://www.google.com/')
     return req.cookies.get_dict()
 
-@app.route('/cookie/analytics', methods=['GET', 'POST'])
+@app.route('/cookie_analytics', methods=['GET', 'POST'])
 def cookie_analytics():
-    req=requests.get('https://analytics.google.com/analytics/web/?hl=fr#/p345017433/reports/reportinghub')
+    req=requests.get('https://analytics.google.com/analytics/web/#/report-home/a250995206p345045242')
     return req.text
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost", port=8000, debug=True)
